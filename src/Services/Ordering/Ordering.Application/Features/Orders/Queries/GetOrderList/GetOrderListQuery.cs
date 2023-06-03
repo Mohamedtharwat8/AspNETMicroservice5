8133128b5bbf821fc9708fb0711bb3a888f35305
Mohamedtharwat8 <m.tharwat897@gmail.com>
2023-06-03 11:02:57 +0300

    Developing Application /Domain Layers

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrderList
 {
-    public  class GetOrderListQuery : IRequest
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
     {
         public string UserName { get; set; }
 
-        public GetOrderListQuery(string userName)
+        public GetOrdersListQuery(string userName)
         {
-            UserName = userName
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
         }
     }
 }
